8dc3014df0702ba758ebacf1c956de9465bc9c5f
build-1000 <77743404+build-1000@users.noreply.github.com>
2021-11-10 21:17:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@
 # Clean existing build using "uninstall.sh".
 
 # Go into the directory where this bash script is contained.
+# doesn't work in windows, just enter director name instead when running the script
 cd `dirname $0`
 
 # Build (compile) the project and store output in the dedicated output directory.
-mkdir -p build
-cd build
+# mkdir -p doesn't work in windows
+# mkdir -p build
+mkdir ~/Library/build
+# cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j4
 
